daf0c830e7baf1563accfa39494e0787fa6cfcf5
Yannick Guegan <y90.guegan@gmail.com>
2020-04-19 19:32:45 +0100

    improvement on javascript

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,90 @@
 class CalculatorView {
 
     updateView(calculatorModel) {
-        document.getElementsByClassName("summary-calc")[0].innerHTML = calculatorModel.operation;
+        document.getElementsByClassName("summary-calc")[0].innerHTML = calculatorModel.currentOperation;
         document.getElementsByClassName("result-calc")[0].innerHTML = calculatorModel.result;
     }
 };
 
 class CalculatorModel {
-
     init() {
-        this.operation = "";
+        this.operandOne = "0";
+        this.operandTwo = "0";
+        this.currentOperation = "0";
         this.result = 0;
     }
-
-
 };
 
 class CalculatorCtrl {
+
     constructor(calculatorModel, calculatorView) {
         this.calculatorModel = calculatorModel;
         this.calculatorView = calculatorView;
     }
     init() {
-        calculatorModel.init();
-        this.operation = "";
+        this.calculatorModel.init();
+        this.operationsToTrigger = null;
+        this.currentOperand = "0";
     }
 
     addToTheOperation(value) {
-        this.calculatorModel.operation += value;
+        this.currentOperand += value;
+        if(this.calculatorModel.currentOperation === "0") {
+            this.calculatorModel.currentOperation = value;
+        }
+        else {
+            this.calculatorModel.currentOperation += value;
+        }
         this.calculatorView.updateView(this.calculatorModel);
     }
 
+    resetCurrentOperand() {
+        this.currentOperand = ""
+    }
+
+    setOperationToTrigger(element, operation) {
+        if(!isNaN(this.currentOperand)) {
+            this.operationsToTrigger = operation;
+            this.calculatorModel.operandOne = this.currentOperand;
+            this.calculatorModel.currentOperation += " " + element.innerText + " ";
+            this.resetCurrentOperand();
+            this.calculatorView.updateView(this.calculatorModel);
+        };
+    }
+
+    add(valueOne, valueTwo) {
+        return Number(valueOne) + Number(valueTwo)
+    }
+
+    substract(valueOne, valueTwo) {
+        return Number(valueOne) - Number(valueTwo)
+    }
+
+    multipleBy(valueOne, valueTwo) {
+        return Number(valueOne) * Number(valueTwo)
+    }
+
+    dividedBy(valueOne, valueTwo) {
+        if(Number(valueTwo) !== 0) {
+            return Number(valueOne) / Number(valueTwo)
+        }
+        return 0.0;
+    }
+
     computeResult() {
-        this.calculatorModel.result = eval(this.calculatorModel.operation);
-        this.calculatorModel.operation += " =";
+        this.calculatorModel.currentOperation += " = ";
+        if(this.operationsToTrigger != null && !isNaN(this.currentOperand)) {
+            if(this.currentOperand !== "") {
+                this.calculatorModel.operandTwo = this.currentOperand;
+            }
+            this.calculatorModel.result = this.operationsToTrigger(this.calculatorModel.operandOne, this.calculatorModel.operandTwo);
+            this.resetCurrentOperand();
+        };
+        this.calculatorView.updateView(this.calculatorModel);
+    }
+
+    resetCalculation() {
+        this.calculatorModel.init();
         this.calculatorView.updateView(this.calculatorModel);
     }
 };
